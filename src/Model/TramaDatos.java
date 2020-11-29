3092a876d79f390674cbe0593b45d2b3e5282d1f
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2020-11-29 18:14:44 -0500

    CRUD started

--- src/Model/TramaDatos.java
+++ src/Model/TramaDatos.java
@@ -10,7 +10,44 @@ import java.time.LocalDate;
  *
  * @author smggu
  */
-public class TramaDatos {
+public class TramaDatos implements Cloneable {
     private int id_toma, id_sensor, valor_tomado;
     private LocalDate fecha_toma;
+    
+    public int getId_toma() {
+        return id_toma;
+    }
+
+    public void setId_toma(int id_toma) {
+        this.id_toma = id_toma;
+    }
+
+    public int getId_sensor() {
+        return id_sensor;
+    }
+
+    public void setId_sensor(int id_sensor) {
+        this.id_sensor = id_sensor;
+    }
+
+    public int getValor_tomado() {
+        return valor_tomado;
+    }
+
+    public void setValor_tomado(int valor_tomado) {
+        this.valor_tomado = valor_tomado;
+    }
+
+    public LocalDate getFecha_toma() {
+        return fecha_toma;
+    }
+
+    public void setFecha_toma(LocalDate fecha_toma) {
+        this.fecha_toma = fecha_toma;
+    }
+    
+    public TramaDatos clone() throws CloneNotSupportedException{
+        TramaDatos cloned = (TramaDatos) super.clone();
+        return cloned;
+    }
 }
---
+++ src/Model/TramaDatosCRUD.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author smggu
+ */
+public class TramaDatosCRUD {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("BONO_DBU");
+    
+    public void insertar(TramaDatos trama){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try{
+            em.persist(trama);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public boolean eliminar(TramaDatos trama){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean done = false;
+        try{
+            em.remove(trama);
+        } catch(Exception e){
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally{
+            em.close();
+            return done;
+        }
+    }
+    
+    public TramaDatos leerSingle(TramaDatos trama){
+        EntityManager em = emf.createEntityManager();
+        TramaDatos res = null;
+        Query q = em.createQuery("SELECT t FROM TramaDatos t " + 
+                                "WHERE  t.id_toma LIKE :id_toma" + 
+                                " AND t.id_sensor LIKE :id_sensor")
+                                .setParameter("id_toma", trama.getId_toma())
+                                .setParameter("id_sensor", trama.getId_sensor());
+        
+        try{
+            res = (TramaDatos)q.getSingleResult();
+        } catch(NonUniqueResultException e){
+            res = (TramaDatos)q.getResultList().get(0);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            em.close();
+            return res;
+        }
+    }
+    
+    public ArrayList<TramaDatos> leerMultiple(){
+        //TO DO
+        return null;
+    } 
+    
+    public boolean actualizar(TramaDatos t, TramaDatos new_t){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean done = false;
+        
+        try{
+            t = leerSingle(t);
+            t = (TramaDatos)new_t.clone();
+            em.merge(t);
+            em.getTransaction().commit();
+            done = true;
+            
+        }catch (Exception e){
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally{
+            em.close();
+            return done;
+        }
+    }
+}
